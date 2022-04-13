51f5f298870e45a2121376fdfc37ee9180f5f501
Ana Matei <anamatei.099@gmail.com>
2022-04-13 15:10:16 +0100

    Fixed Home

---
+++ ts-project/src/components/DetailsPage.tsx
@@ -0,0 +1,49 @@
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { TrackDetail } from '../interfaces'
+import { Container, Row, Image, Col } from 'react-bootstrap'
+
+
+
+const DetailsPage =() => {
+
+const { id }=useParams()
+
+const [trackDetails, setTrackDetails] = useState<TrackDetail | null>(null)
+
+
+  useEffect(() => {
+    fetchTracks()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+
+
+const fetchTracks = async () => {
+    const response = await fetch(
+      'https://striveschool-api.herokuapp.com/api/deezer/track/' + id
+    )
+
+    if (!response.ok) return
+
+    const data = await response.json()
+    setTrackDetails(data)
+  }
+
+
+    return trackDetails ? 
+    (
+ 
+        <Container>
+        <Row>
+          <Col xs={10}>
+            <Image fluid src={trackDetails.album.cover_big} />
+            <h4>{trackDetails.title}</h4>
+            <h6>{trackDetails.artist.name}</h6>
+          </Col>
+        </Row>
+      </Container>
+    ) : null
+}
+
+export default DetailsPage
---
+++ ts-project/src/components/MainPage.tsx
@@ -0,0 +1,61 @@
+import { Container, Form, Navbar,Nav, Row, Col, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { Track } from "../interfaces";
+import { ChangeEvent, FormEvent, useState } from "react";
+import "./mainpage.css";
+
+const MainPage = () => {
+  const [input, setInput] = useState("");
+  const [results, setResults] = useState<Track[]>([]);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    const response = await fetch(
+      "https://striveschool-api.herokuapp.com/api/deezer/search?q=" + input
+    );
+
+    if (response.ok) {
+      const { data } = await response.json();
+      setResults(data);
+    }
+  };
+
+  return (
+    <>
+      <Container fluid className="container-wrapper">
+        <Navbar>
+          <Nav>
+              <Form onSubmit={handleSubmit}>
+                <Form.Control type="search" value={input} onChange={handleChange} />
+              </Form>
+          </Nav>
+        </Navbar>
+
+        <Row className="my-3 row-wrapper">
+          {results
+            .map((track) => (
+              <Col xs={10} md={3} className="mt-3" id="col-wrapper">
+                <Card>
+                  <Link to={`details/${track.id}`} style={{ color: "white" }}>
+                    <Card.Img variant="top" src={track.album.cover_medium} />
+                  </Link>
+                  <Card.Body>
+                    <Card.Title>{track.title}</Card.Title>
+                    <Card.Text>{track.artist.name}</Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))
+            .splice(0, 9)}
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default MainPage;
